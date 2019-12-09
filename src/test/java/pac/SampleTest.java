37dd686265ad446f9faad0abce93177329f252f0
Reddy <reddysathyasree@yahoo.com>
2019-12-09 19:31:24 +0530

    first comiit

---
+++ src/test/java/pac/SampleTest.java
@@ -0,0 +1,27 @@
+package pac;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	
+	@Test
+	public void test1() {
+		System.out.println("execute test1");
+	}
+
+	
+	@Test
+	public void test2() {
+		System.out.println("execute test2");
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("execute test3");
+	}
+	
+	@Test
+	public void test4() {
+		System.out.println("execute test4");
+	}
+}
